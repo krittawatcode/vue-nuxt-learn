3a73aaf5f94c38f177d5b488e45378ca0e8064cd
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-10-06 10:57:41 +0700

    add component to layout

--- my-first-nuxt-app/layouts/users.vue
+++ my-first-nuxt-app/layouts/users.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
+      <Header />
     <header>User Section</header>
     <nuxt />
   </div>
 </template>
+
+<script>
+import Header from '@/components/Header'
+
+export default {
+    components : {
+        Header
+    }
+}
+</script>
