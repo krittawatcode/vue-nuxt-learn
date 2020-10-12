e76ef0b5fe23f7fa4f74da24fc330fd4dc3ddfb8
Krittawat Wisedsuwon <krittawat.code@gmail.com>
2020-10-12 10:35:36 +0700

    add new post part in admin

--- my-first-blog/.nuxt/router.js
+++ my-first-blog/.nuxt/router.js
@@ -4,7 +4,11 @@ import { interopDefault } from './utils'
 import scrollBehavior from './router.scrollBehavior.js'
 
 const _5cf1973f = () => interopDefault(import('../pages/about/index.vue' /* webpackChunkName: "pages/about/index" */))
+const _69f72c81 = () => interopDefault(import('../pages/admin/index.vue' /* webpackChunkName: "pages/admin/index" */))
 const _3685d005 = () => interopDefault(import('../pages/posts/index.vue' /* webpackChunkName: "pages/posts/index" */))
+const _bd3101e8 = () => interopDefault(import('../pages/admin/auth/index.vue' /* webpackChunkName: "pages/admin/auth/index" */))
+const _7085b15e = () => interopDefault(import('../pages/admin/new-post/index.vue' /* webpackChunkName: "pages/admin/new-post/index" */))
+const _1c2a1ea8 = () => interopDefault(import('../pages/admin/_postId/index.vue' /* webpackChunkName: "pages/admin/_postId/index" */))
 const _563d6070 = () => interopDefault(import('../pages/posts/_id/index.vue' /* webpackChunkName: "pages/posts/_id/index" */))
 const _2b372881 = () => interopDefault(import('../pages/index.vue' /* webpackChunkName: "pages/index" */))
 
@@ -29,9 +33,25 @@ export const routerOptions = {
     component: _5cf1973f,
     name: "about"
   }, {
+    path: "/admin",
+    component: _69f72c81,
+    name: "admin"
+  }, {
     path: "/posts",
     component: _3685d005,
     name: "posts"
+  }, {
+    path: "/admin/auth",
+    component: _bd3101e8,
+    name: "admin-auth"
+  }, {
+    path: "/admin/new-post",
+    component: _7085b15e,
+    name: "admin-new-post"
+  }, {
+    path: "/admin/:postId",
+    component: _1c2a1ea8,
+    name: "admin-postId"
   }, {
     path: "/posts/:id",
     component: _563d6070,
